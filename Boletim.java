eeeab0910fbb8431c243494004d984411eb1de55
João <joaosantanaguilherme@gmail.com>
2026-06-23 20:57:07 -0300

    feat: adição da classe boletim com método de cálculo da média aritmética de duas notas como parâmetro

---
+++ Boletim.java
@@ -0,0 +1,15 @@
+public class Boletim {
+    double nota1;
+    double nota2;
+    double media_final;
+
+
+    public double Media(double nota1, double nota2){
+
+        media_final = (nota1+nota2)/2;
+
+        return media_final;
+
+
+    }
+}
